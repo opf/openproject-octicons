627659242fe4c5792b286a8a0f2b166cb5be912b
Alexander Brandon Coles <a.coles@openproject.com>
2025-12-06 18:12:27 -0300

    Autocorrect eslint errors in helpers.ts

--- lib/octicons_angular/src/helpers.ts
+++ lib/octicons_angular/src/helpers.ts
@@ -1,11 +1,9 @@
 export type SVGSize = 'xsmall'|'small'|'medium'|'large';
 
-export interface SVGData {
- [key: string]: {
-   width: number,
-   paths: string[],
- };
-};
+export type SVGData = Record<string, {
+   width:number,
+   paths:string[],
+ }>;;
 
 export const sizeMap = {
   xsmall: 12,
@@ -17,12 +15,12 @@ export const sizeMap = {
 export function closestNaturalHeight(naturalHeights:string[], height:number) {
   return naturalHeights
     .map(naturalHeight => parseInt(naturalHeight, 10))
-    .reduce((acc, naturalHeight) => (naturalHeight <= height ? naturalHeight : acc), parseInt(naturalHeights[0], 10))
+    .reduce((acc, naturalHeight) => (naturalHeight <= height ? naturalHeight : acc), parseInt(naturalHeights[0], 10));
 }
 
 export function toDOMString(data:SVGData, size:SVGSize = 'medium', extraAttributes:Record<string, string> = {}) {
   const height = sizeMap[size];
-  const naturalHeight = closestNaturalHeight(Object.keys(data), height)
+  const naturalHeight = closestNaturalHeight(Object.keys(data), height);
   const { width, paths } = data[naturalHeight.toString()];
   const elWidth =  height * (width / naturalHeight);
   return `<svg
@@ -30,6 +28,6 @@ export function toDOMString(data:SVGData, size:SVGSize = 'medium', extraAttribut
     style="height: ${height}px; width: ${elWidth}px;"
     ${Object.keys(extraAttributes).reduce((total, attr) => `${total} ${attr}="${extraAttributes[attr]}"`, '')}
   >
-    ${paths.map(p => `<path d="${p}"></path>`)}
-  </svg>`
+    ${paths.map(p => `<path d="${p}"></path>`).join('')}
+  </svg>`;
 }
